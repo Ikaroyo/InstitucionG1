489b50b0315726201a74db4c4ea50e951f7f02c5
Barbi Borrello <biborrello@hotmail.com>
2022-05-15 11:13:11 -0300

    Todas las clases data guardan exitosamente. Pendientes: modificar, borrar y listar

--- src/institucion/Controlador/InscripcionData.java
+++ src/institucion/Controlador/InscripcionData.java
@@ -30,21 +30,18 @@ public class InscripcionData {
     }
 
     public void inscribir(Inscripcion insc) {
-        String sql = "INSERT INTO inscripcion (id_alumno, id_materia, nota) VALUES (?, ?, ?)";
+        String sql = "INSERT INTO inscripcion (idalumno, idmateria, nota) VALUES (?, ?, ?)";
 
         try {
 
             PreparedStatement ps = con.prepareStatement(sql, Statement.RETURN_GENERATED_KEYS);
-            JOptionPane.showMessageDialog(null, "llegue hasta aca");
+
             ps.setInt(1, insc.getAlumno().getIdAlumno());
-            JOptionPane.showMessageDialog(null, "llegue hasta aca 2");
             ps.setInt(2, insc.getMateria().getIdMateria());
-            JOptionPane.showMessageDialog(null, "llegue hasta aca 3");
             ps.setDouble(3, insc.getNota());
-            JOptionPane.showMessageDialog(null, "llegue hasta aca 4");
 
             ps.executeUpdate();
-            JOptionPane.showMessageDialog(null, "llegue hasta aca 5");
+
             ResultSet rs = ps.getGeneratedKeys();
             if (rs.next()) {
                 insc.setIdInscripcion(rs.getInt(1));
