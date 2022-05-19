07ed5a2d29d04d6948819ca6d600c851ac44ff77
Barbi Borrello <biborrello@hotmail.com>
2022-05-18 22:31:32 -0300

    Actualizacion 18/5/2022 -22:31

--- src/institucion/Controlador/InscripcionData.java
+++ src/institucion/Controlador/InscripcionData.java
@@ -23,19 +23,22 @@ import javax.swing.JOptionPane;
  */
 public class InscripcionData {
 
-    private Connection con = null;
+     private Connection con = null;
+     private Conexion conexion;
 
     public InscripcionData(Conexion conexion) {
         try {
-            con = conexion.getConexion();
+            this.conexion=conexion;
+            this.con = conexion.getConexion();
         } catch (SQLException ex) {
-            System.out.println("Error en la conexion");
+            System.out.println("Error al abrir al obtener la conexion");
         }
     }
 
+
         public Alumno buscarAlumno(int idAlumno){
     
-        AlumnoData ad=new AlumnoData(con);
+        AlumnoData ad=new AlumnoData(conexion);
         
         return ad.buscarAlumno(idAlumno);
         
@@ -43,14 +46,14 @@ public class InscripcionData {
     
         public Materia buscarMateria(int idMateria){
     
-        MateriaData md=new MateriaData(con);
+        MateriaData md=new MateriaData(conexion);
         return md.buscarMateria(idMateria);
     
     }
 
 
     public void guardarInscripcion(Inscripcion insc) {
-        String sql = "INSERT INTO inscripcion (idalumno, idmateria, nota) VALUES (?, ?, ?)";
+        String sql = "INSERT INTO inscripcion (idAlumno, idMateria, nota) VALUES (?, ?, ?)";
 
         try {
 
@@ -63,6 +66,7 @@ public class InscripcionData {
             ps.executeUpdate();
 
             ResultSet rs = ps.getGeneratedKeys();
+            
             if (rs.next()) {
                 insc.setIdInscripcion(rs.getInt(1));
                 JOptionPane.showMessageDialog(null, "Inscripcion exitosa");
@@ -85,7 +89,7 @@ public class InscripcionData {
             
 
         try {
-            String sql = "SELECT * FROM cursada;";
+            String sql = "SELECT * FROM inscripcion;";
             PreparedStatement statement = con.prepareStatement(sql);
             
             ResultSet resultSet = statement.executeQuery();
@@ -93,7 +97,7 @@ public class InscripcionData {
             
             while(resultSet.next()){
                 inscripcion = new Inscripcion();
-                inscripcion.setIdInscripcion(resultSet.getInt("id"));
+                inscripcion.setIdInscripcion(resultSet.getInt("idInscripcion"));
                 
                 Alumno a= buscarAlumno(resultSet.getInt("idAlumno"));
                 inscripcion.setAlumno(a);
@@ -119,7 +123,7 @@ public class InscripcionData {
             ArrayList<Inscripcion> inscripciones = new ArrayList<>();   
 
         try {
-            String sql = "SELECT * FROM cursada WHERE idAlumno = ?;";
+            String sql = "SELECT * FROM inscripcion WHERE idAlumno = ?;";
              
             PreparedStatement statement = con.prepareStatement(sql);
             statement.setInt(1,id);
@@ -131,7 +135,7 @@ public class InscripcionData {
             while(resultSet.next()){
                 
                 inscripcion = new Inscripcion();
-                inscripcion.setIdInscripcion(resultSet.getInt("id"));
+                inscripcion.setIdInscripcion(resultSet.getInt("idInscripcion"));
                 
                 Alumno a=buscarAlumno(resultSet.getInt("idAlumno"));
                 inscripcion.setAlumno(a);
@@ -157,8 +161,8 @@ public class InscripcionData {
             
 
         try {
-            String sql = "SELECT idMateria, nombre FROM cursada, materia WHERE cursada.idMateria = materia.id\n" +
-            "and cursada.idAlumno = ?;";
+            String sql = "SELECT inscripcion.idMateria, nombre FROM inscripcion, materia WHERE inscripcion.idMateria = materia.idMateria\n" +
+            "and inscripcion.idAlumno = ?;";
             PreparedStatement statement = con.prepareStatement(sql);
             statement.setInt(1, id);
             ResultSet resultSet = statement.executeQuery();
@@ -168,7 +172,9 @@ public class InscripcionData {
                 materia.setIdMateria(resultSet.getInt("idMateria"));
                 materia.setNombre(resultSet.getString("nombre"));
                 materias.add(materia);
-            }      
+                
+               JOptionPane.showMessageDialog(null,"El alumno esta inscripto en :" +" "+materia.getNombre());
+            }   
             statement.close();
         } catch (SQLException ex) {
             System.out.println("Error al obtener los alumnos: " + ex.getMessage());
@@ -184,17 +190,18 @@ public class InscripcionData {
             
 
         try {
-            String sql = "Select * from materia where id not in "
-                    + "(select idMateria from cursada where idAlumno =?);";
+            String sql = "Select * from materia where idMateria not in "
+                    + "(select idMateria from inscripcion where idAlumno =?);";
             PreparedStatement statement = con.prepareStatement(sql);
             statement.setInt(1, id);
             ResultSet resultSet = statement.executeQuery();
             Materia materia;
             while(resultSet.next()){
                 materia = new Materia();
-                materia.setIdMateria(resultSet.getInt("id"));
+                materia.setIdMateria(resultSet.getInt("idMateria"));
                 materia.setNombre(resultSet.getString("nombre"));
                 materias.add(materia);
+                JOptionPane.showMessageDialog(null,"El no alumno esta inscripto en :" +" "+materia.getNombre());
             }      
             statement.close();
         } catch (SQLException ex) {
@@ -210,7 +217,7 @@ public class InscripcionData {
     
         try {
             
-            String sql = "DELETE FROM cursada WHERE idAlumno =? and idMateria =?;";
+            String sql = "DELETE FROM inscripcion WHERE idAlumno =? and idMateria =?;";
 
             PreparedStatement statement = con.prepareStatement(sql);
             statement.setInt(1, idAlumno);
@@ -219,7 +226,7 @@ public class InscripcionData {
             
             statement.executeUpdate();
             
-            
+           JOptionPane.showMessageDialog(null,"Se borro al alumno");
             statement.close();
     
         } catch (SQLException ex) {
@@ -234,7 +241,7 @@ public class InscripcionData {
     
         try {
             
-            String sql = "UPDATE cursada SET nota = ? WHERE idAlumno =? and idMateria =?;";
+            String sql = "UPDATE inscripcion SET nota = ? WHERE idAlumno =? and idMateria =?;";
 
            PreparedStatement statement = con.prepareStatement(sql);
             statement.setInt(1,nota);
@@ -244,7 +251,7 @@ public class InscripcionData {
             
             statement.executeUpdate();
             
-            
+            JOptionPane.showMessageDialog(null,"Nota actualizada");
             statement.close();
     
         } catch (SQLException ex) {
--- src/institucion/Modelo/InstitucionTEST.java
+++ src/institucion/Modelo/InstitucionTEST.java
@@ -33,23 +33,27 @@ public class InstitucionTEST {
             Conexion conexion = new Conexion();
             AlumnoData ad = new AlumnoData(conexion);
             MateriaData md = new MateriaData(conexion);
-            InscripcionData id1 = new InscripcionData(conexion);
+            InscripcionData id = new InscripcionData(conexion);
+            
+//-----------------------------------------------------------------------------------------------------
 
-//            Alumno alumno1 = new Alumno("Fariolli", "Santiago", LocalDate.of(1987, 7, 13), true);
+// SE CREA SIN ID = CLASES MODELO // SE RELACIONA POR ID = INSCRIPCION DATA//
+
+            Alumno alumno1 = new Alumno(2,"Fariolli", "Santiago", LocalDate.of(1987, 7, 13), true);
 
 //            System.out.println(alumno1);
 //            ad.guardarAlumno(alumno1);
 
-//            Alumno alumno2 = new Alumno("Borrello", "Barbara", LocalDate.of(1977, 2, 9), true);
+            Alumno alumno2 = new Alumno(4,"Borrello", "Barbara", LocalDate.of(1977, 2, 9), true);
 //            System.out.println(alumno2);
 //            ad.guardarAlumno(alumno2);
 
 
-//            Alumno alumno3 = new Alumno("Borrello", "Hector", LocalDate.of(1949, 1, 5), true);
+            Alumno alumno3 = new Alumno(5,"Borrello", "Hector", LocalDate.of(1949, 1, 5), true);
 //            System.out.println(alumno3);
 //            ad.guardarAlumno(alumno3);
 //
-//            Alumno alumno4 = new Alumno("Lopez", "Jesica", LocalDate.of(1989, 5, 30), false);
+            Alumno alumno4 = new Alumno(6,"Lopez", "Jesica", LocalDate.of(1989, 5, 30), false);
 //            System.out.println(alumno4);
 //            ad.guardarAlumno(alumno4);
 
@@ -70,24 +74,25 @@ public class InstitucionTEST {
 //     
 //             System.out.println(it.hasNext());
 //             }
-//-------------------------------------------------------------------------------------------------------------              
-//             Materia m = new Materia("Matematica",1, true);
+//-------------------------------------------------------------------------------------------------------------
+
+             Materia m1 = new Materia(1,"Matematica",1, true);
 //             System.out.println(m1);
 //             md.guardarMateria(m1);
 //
-//             Materia m2 = new Materia("Web",2, true);
+             Materia m2 = new Materia(2,"Web",2, true);
 //             System.out.println(m2);
 ////           md.guardarMateria(m2);
 //
-//             Materia m3 = new Materia("Laboratorio",2, true);
+             Materia m3 = new Materia(3,"Laboratorio",2, true);
 //             System.out.println(m3);
 //             md.guardarMateria(m3);
-//
-//             Materia m4 = new Materia("Ingles",1, true);
+////
+             Materia m4 = new Materia(4,"Ingles",1, true);
 //             System.out.println(m4);
 //             md.guardarMateria(m4);
 //
-//             Materia m5 = new Materia("Eda",2, false);
+             Materia m5 = new Materia(5,"Eda",2, false);
 //             System.out.println(m5);
 //             md.guardarMateria(m5);
 //
@@ -118,11 +123,67 @@ public class InstitucionTEST {
 //              md.modificarMateria(new Materia ("Laboratorio 2",3,true) , 6);
 //-------------------------------------------------------------------------------------------------------------        
 
+//               Inscripcion inscripcion1 = new Inscripcion(alumno1,m3,0);
+//               id.guardarInscripcion(inscripcion1);
+//
+//               Inscripcion inscripcion2 = new Inscripcion(alumno2,m4,0);
+//               id.guardarInscripcion(inscripcion2);
+//
+//               Inscripcion inscripcion3 = new Inscripcion(alumno3,m2,0);
+//               id.guardarInscripcion(inscripcion3);
+//                
+//               Inscripcion inscripcion4 = new Inscripcion(alumno4,m1,0);
+//               id.guardarInscripcion(inscripcion4);
+//
+//                
+//                System.out.println(id.obtenerInscripciones());
+//                ArrayList<Inscripcion> inscripciones = new ArrayList<>();
+//       
+//  
+//                Iterator <Inscripcion> it = inscripciones.iterator();
+//
+//                while(it.hasNext()){
+//     
+//                System.out.println(it.hasNext());
+////                }
+//
+//                System.out.println(id.obtenerInscripcionesXAlumno(2));
+//                ArrayList<Inscripcion> inscripciones = new ArrayList<>();
+//       
+//  
+//                 Iterator <Inscripcion> it = inscripciones.iterator();
+//
+//                 while(it.hasNext()){
+//     
+//                 System.out.println(it.hasNext());
+//                 }
+//
+//                  System.out.println(id.obtenerMateriasCursadas(2));
+//                 ArrayList<Inscripcion> inscripciones = new ArrayList<>();
+//       
+//  
+//                  Iterator <Inscripcion> it = inscripciones.iterator();
+//
+//                  while(it.hasNext()){
+//     
+//                  System.out.println(it.hasNext());
+////                 }
+//
+//                  System.out.println(id.obtenerMateriasNOCursadas(2));
+//                 ArrayList<Inscripcion> inscripciones = new ArrayList<>();
+//       
+//  
+//                  Iterator <Inscripcion> it = inscripciones.iterator();
+//
+//                  while(it.hasNext()){
+//     
+//                  System.out.println(it.hasNext());
+//                 }
+//
+//                   id.borrarInscripcionDeUnaMateriaDeunAlumno(4, 4);
 
-                
+                     id.actualizarNotaCursada(2, 3, 10);
 
-
-              
 
             
 //
